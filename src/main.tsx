6861998276c128d27e027492a4fdc866dae7fef1
sreevasaviAduri <48317606+sreevasaviAduri@users.noreply.github.com>
2025-04-21 21:24:17 +0200

    fix path

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from "aws-amplify";
-import outputs from "./amplify_outputs.json";
+import outputs from "../src/amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
